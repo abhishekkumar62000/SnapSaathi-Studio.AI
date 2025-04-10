9c3b914225879d6472103b1f04ad12bc5ad87fff
Abhishek Yadav <159156426+abhishekkumar62000@users.noreply.github.com>
2025-04-10 21:09:31 +0530

    Update App.py

--- App.py
+++ App.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import cv2 
-import numpy as np
+import numpy as np 
 from PIL import Image, ImageEnhance, ImageOps, ImageDraw, ImageFont 
 from PIL.ExifTags import TAGS  # For EXIF metadata 
 import io
